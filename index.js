d10d016344d569a4a1807980d23c4bba0909bf86
agnoStack <103324986+Agboola2004@users.noreply.github.com>
2022-08-30 12:46:06 +0100

    linked list

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//STACKS
+
 let stack = function () {
   this.count = 0;
   this.storage = {};
@@ -25,6 +27,81 @@ let stack = function () {
   };
 
   this.peek = function () {
-    this.storage[this.count - 1];
+    return this.storage[this.count - 1];
   };
 };
+
+let myStack = new stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+myStack.push("ali");
+myStack.push("aliii");
+myStack.push("aliii");
+myStack.push("aliiii");
+console.log(myStack.pop());
+console.log(myStack.size());
+
+//ACADEMIND DATA STRUCTURE AND ALGORITHM
+//SETS
+const set = new Set();
+
+//LINKED LIST DATA STRUCTURE
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  append(value) {
+    const newNode = { value: value, next: null };
+    if (this.tail) {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+    if (!this.head) {
+      this.head = newNode;
+    }
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return; // return null;
+    }
+
+    let curNode = this.head;
+    while (curNode){
+
+    }
+  }
+  
+  prepend(value) {
+    const newNode = { value: value, next: this.head };
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+  }
+
+  toArray() {
+    const elements = [];
+    let curNode = this.head;
+    while (curNode) {
+      elements.push(curNode);
+      curNode = curNode.next;
+    }
+    return elements;
+  }
+}
+
+const LinkedList1 = new LinkedList();
+
+LinkedList1.append("hey");
+LinkedList1.append(true);
+LinkedList1.append(12.85);
+LinkedList1.prepend("first value");
+
+console.log(LinkedList1.toArray());
